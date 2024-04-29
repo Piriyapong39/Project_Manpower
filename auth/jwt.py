b23b9c5a22a49c1ef49791c6d0d24f3726582625
Piriyapong Trakanjun <150586948+Piriyapong39@users.noreply.github.com>
2024-04-30 03:25:28 +0700

    Update jwt.py

--- auth/jwt.py
+++ auth/jwt.py
@@ -19,12 +19,12 @@ def signJWT(userID: str):
         "userID": userID,
         "expiry": time.time() + 600
     }
-    token = jwt.encode(payload, JWT_SECRET, algorithm = JWT_ALGORITHM)
+    token = jwt.encode(payload, JWT_SECRET, algorithm = "HS256")
     return token_response(token)
 
 def decodeJWT(token: str):
     try:
-        decode_token = jwt.decode(token, JWT_SECRET, algorithm = JWT_ALGORITHM)
+        decode_token = jwt.decode(token, JWT_SECRET, algorithm = "HS256")
         return decode_token if decode_token['expires'] >= time.time() else None
     except:
         return {}
